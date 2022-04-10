52265e5dcea130a1449e7df1d105bc687d7c5ef6
SanniSys <103313066+SanniSys@users.noreply.github.com>
2022-04-10 22:02:19 +0200

    initial state

--- main.py
+++ main.py
@@ -2,10 +2,23 @@ import tkinter as tk
 
 # create main-window object
 window = tk.Tk()
+window.title("My Title")  # Set a title for main Window
+window.geometry("800x400")  # Set width x height
+window.minsize(width=250, height=250)  # cannot change window to below value
+# window.maxsize(width=400, height=800)
+# window.resizable(width=False, height=False) # changes possible depending on True/False
 
-# create label in main window
-label_greet = tk.Label(window, text='Welcome!')
+# label widged:
+# create label in main window, bg for background fg = foreground
+label_greet = tk.Label(window, text='Welcome!', bg="red")
+label_greet2 = tk.Label(window, text='Welcome, my friend!', bg="green")
 
-label_greet.pack()
+# Layout-manager, pack grid or place
+# pack does a lot of things automatically....
+label_greet.pack(fill="x")  # every object has to be added
+label_greet2.pack(side="bottom", expand=True, fill="y")  # expand uses max space
+# fill= "x" / "y" / "both"
+# side="top"(standart) / "bottom" / "left" / "right"
 
+# Eventloop checking for occurring events on window
 window.mainloop()

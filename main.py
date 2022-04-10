39eb2cc56fa934f619943c82f93cf04250868d0a
SanniSys <103313066+SanniSys@users.noreply.github.com>
2022-04-10 14:08:04 +0200

    initial state

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Hello Git")
-print("Hello World")
+import tkinter as tk
+
+# create main-window object
+window = tk.Tk()
+
+# create label in main window
+label_greet = tk.Label(window, text='Welcome!')
+
+label_greet.pack()
+
+window.mainloop()
